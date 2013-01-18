18efd68d24c7bac8436cf500a939ee1f19bf4c2f
Jimmy Tang <jtang@tchpc.tcd.ie>
2013-01-18 12:56:55 +0000

    Ignore proxy settings from system, not setting this causes rsolr to fail.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+ENV_JAVA["http.proxyHost"] = nil
+ENV_JAVA["https.proxyHost"] = nil
+
 # Initialize the rails application
 NuigRnag::Application.initialize!
